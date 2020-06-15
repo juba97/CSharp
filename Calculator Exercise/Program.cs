6dc649b06ca630870c54e8967b986a999184d85a
Juba's Macbook <jubakoguashvili@MacBook-Pro.local>
2020-06-15 12:11:09 +0400

    CalculatorExercise

---
+++ Calculator Exercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculatori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First Number");
+            double Number = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            double Number2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Symbol Operator");
+            char Symbol = char.Parse(Console.ReadLine());
+            
+            if (Symbol == '+' || Symbol == '-' || Symbol == '*' || Symbol == '/' || Symbol == '%')
+            {
+                
+            if (Symbol == '+')
+            {
+                System.Console.WriteLine($"{Number} + {Number2} = {Number + Number2}");
+            }
+            if (Symbol == '-')
+            {
+                System.Console.WriteLine($"{Number} - {Number2} = {Number - Number2}");
+            }
+            if (Symbol == '*')
+            {
+                System.Console.WriteLine($"{Number} * {Number2} = {Number * Number2}");
+            }
+            if (Symbol == '/')
+            {
+                System.Console.WriteLine($"{Number} / {Number2} = {Number / Number2}");
+            }
+            if (Symbol == '%')
+            {
+                System.Console.WriteLine($"{Number} % {Number2} = {Number % Number2}");
+            }
+            }
+            else
+            {
+                System.Console.WriteLine("Error");
+            }
+        }
+    }
+}
