905b09185964e0e55819166789a9578dbfe2bcc0
Juba's Macbook <jubakoguashvili@MacBook-Pro.local>
2020-06-15 11:46:37 +0400

    FirstExercise

---
+++ FirstExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int z = 4;
+            // int b = 10;
+            // GetMaxValue(z, b);
+
+            System.Console.WriteLine(ReturnLengthName("macivari", "skami"));
+        }
+
+
+        public static /*დამაბრუნებელი მნიშვნელობდა => */ int /*ფუნქციის სახელი => */ GetMaxValue(int x, int y) //<= ფუნქციის პარამეტრები
+        {
+            if(x > y)
+            {
+                return x;
+            }
+            else
+            {
+                return y;
+            }
+        }
+
+        public static string ReturnLengthName(string firstName, string secondName)
+        {
+            return $"{firstName} and {secondName}";
+        }
+    }
+}
+
+
