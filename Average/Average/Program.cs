a5a3e06a2f6c0e91dd5d9de63306dd1eac328772
juba97 <64692459+juba97@users.noreply.github.com>
2020-10-24 23:33:54 +0400

    Average

---
+++ Average/Average/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                double sum = 0, avg = 0;
+                double[] numbers = { 2, 3, 4 };
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    sum += numbers[i];
+                }
+                avg = sum / numbers.Length;
+                Console.WriteLine("The Sum is : " + sum);
+                Console.WriteLine("The Average is : " + avg);
+
+               
+            
+        }
+    }
+}
