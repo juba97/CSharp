a6e597f76d259fda2a5d8e1a4df07d9498361c48
juba97 <64692459+juba97@users.noreply.github.com>
2020-10-24 23:29:16 +0400

    MiddleNumber

---
+++ MiddleNumber/MiddleNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace G06_20202310
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Random r = new Random();
+
+            int number = r.Next(100, 1000);
+            string text = number.ToString();
+            char symbol = text[1];
+
+            Console.WriteLine(number);
+            Console.WriteLine(symbol);
+
+            //var x = new Random().Next(100, 1000).ToString()[1];
+            //Console.WriteLine(x);
+
+        }
+
+    }
+
+}
