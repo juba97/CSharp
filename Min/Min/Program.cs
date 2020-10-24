275c69f4ca14ec4eed223b3c6dc37fad8537cb47
juba97 <64692459+juba97@users.noreply.github.com>
2020-10-24 23:22:34 +0400

    Min

---
+++ Min/Min/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace G06_20202310_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random r = new Random();
+            int min;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = r.Next(1, 100);
+                Console.WriteLine(numbers[i]);
+            }
+            min = numbers[0];
+            
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (min > numbers[i])
+                    min = numbers[i];
+                
+            }
+
+            
+            Console.WriteLine("Min number -  " + min);
+
+        }
+    }
+}
+
