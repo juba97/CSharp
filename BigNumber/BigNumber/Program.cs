8a5a185889b18007b570d576ddd0a381effe8953
juba97 <64692459+juba97@users.noreply.github.com>
2020-10-25 17:13:24 +0400

    BigNumber

---
+++ BigNumber/BigNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace G06_20202310_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            Random r = new Random();
+            int max;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = r.Next(1, 100);
+                Console.WriteLine($"numbers[{i}] = {numbers[i]}");
+            }
+
+            max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+
+                if (max < numbers[i])
+                    max = numbers[i];
+            }
+
+            Console.WriteLine("largest number - " + max);
+
+        }
+    }
+}
