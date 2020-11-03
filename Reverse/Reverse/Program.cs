58616036c88faaafed491a70c2041fc5d0c381ed
juba97 <64692459+juba97@users.noreply.github.com>
2020-11-03 15:09:44 +0400

    Reverse

---
+++ Reverse/Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers =
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+            };
+
+            int[] reverse = new int[numbers.Length];
+            int j = numbers.Length - 1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                reverse[i] = numbers[j];
+                j--;
+                Console.WriteLine(reverse[i]);
+            }
+        }
+    }
+}
