85f373859e4728da99eecea8b132d8a9cb26eed6
juba97 <64692459+juba97@users.noreply.github.com>
2020-11-08 14:10:41 +0400

    Method

---
+++ Function/Function/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Maxs = MaxNumber(100, 200);
+            Console.WriteLine($"LargestNumber is - {Maxs}");
+            double Sum = PrintNumber(10.5, 6.3);
+            Console.WriteLine($"Sum is - {Sum}");
+            programmer();
+            int Min = Minnumber(100, 200);
+            Console.WriteLine($"Minnumber is - {Min}");
+        }
+        public static int  MaxNumber(int numbers, int max)
+        {
+                if (numbers > max)
+                {
+                    return numbers;
+                }
+                else
+                {
+                    return max;
+                }                     
+        }
+        public static int Minnumber(int Minnumbers, int Min)
+        {
+            if (Minnumbers < Min)
+            {
+                return Minnumbers;
+            }
+            else
+            {
+                return Min;
+            }
+
+        }
+        public static double PrintNumber(double FirstNumber, double SecondNumber) => FirstNumber + SecondNumber;
+        public static void programmer()
+        {
+            Console.WriteLine("I'm a programmer");
+        }
+    }
+}
