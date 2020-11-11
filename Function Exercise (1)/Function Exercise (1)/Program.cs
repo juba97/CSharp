50379ef36917ffdeabdf448e70edfb99a1a25e48
juba97 <64692459+juba97@users.noreply.github.com>
2020-11-11 21:32:26 +0400

    Exercise (1)

---
+++ Function Exercise (1)/Function Exercise (1)/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ConsoleApp117
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            int[] x = { 3, 4, 5, 6, 7 };
+            PrintSum(x);
+            int[] i = { 3, 4, 5, 6, 7 };
+            int y = PrintAvg(i);
+            Console.WriteLine($"Avg is - {y}");
+            int[] j = { 3, 4, 5, 6, 7 };
+            int b = MaxNumber(j);
+            Console.WriteLine($"MaxNumber is - {b}");
+            int[] a = { 3, 4, 5, 6, 7 };
+            int c = MinNumber(a);
+            Console.WriteLine($"MinNumber is - {c}");
+        }
+
+        static void PrintSum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+
+            }
+            Console.WriteLine($"Sum is - {sum} ");
+
+        }
+
+        static int PrintAvg(int[] array)
+        {
+            int avg = 0, save = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                save += array[i];
+
+            }
+            avg = save / array.Length;
+            return avg;
+        }
+
+        static int MaxNumber(int[] array)
+        {
+            int maxnumber = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > maxnumber)
+                {
+                    maxnumber = array[i];
+                }
+            }
+            return maxnumber;
+        }
+
+        static int MinNumber(int[] array)
+        {
+            int minnumber = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < minnumber)
+                {
+                    minnumber = array[i];
+                }
+            }
+            return minnumber;
+        }
+    }
+
+}
