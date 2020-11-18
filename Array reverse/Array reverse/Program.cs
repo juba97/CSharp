8092d10bb510e46d2c8603f2c44b38307f8478d9
juba97 <64692459+juba97@users.noreply.github.com>
2020-11-18 19:54:24 +0400

    Reverse

---
+++ Array reverse/Array reverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Array_reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1,2,3,4,5};
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                int temp = arr[i];
+                arr[i] = arr[arr.Length - (i + 1)];
+                arr[arr.Length - (i + 1)] = temp;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
