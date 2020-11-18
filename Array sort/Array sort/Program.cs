69ba77c5dbcbea139f486fde45b3725a05431b6c
juba97 <64692459+juba97@users.noreply.github.com>
2020-11-18 19:55:50 +0400

    Add files via upload

---
+++ Array sort/Array sort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 5, 1, 4, 2 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
