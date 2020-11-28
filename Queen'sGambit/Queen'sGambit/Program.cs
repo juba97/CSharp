1cc620b44bf0d3b5913480857403814492f85384
juba97 <64692459+juba97@users.noreply.github.com>
2020-11-28 17:10:21 +0400

    Queen's

---
+++ Queen'sGambit/Queen'sGambit/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Queen_sGambit
+{
+    class Program
+    {
+		public static void Main(string[] args)
+		{
+			Move(0);
+
+		}
+		static int[] x = new int[8];
+		static void Print()
+		{
+			for (int j = 0; j < 8; j++)
+			{
+				Console.Write(x[j] + 1);
+			}
+			Console.Write("\n");
+		}
+		static int Free(int xx, int yy)
+		{
+			for (int i = 0; i < yy; i++)
+			{
+				if ((x[i] == xx) || (Math.Abs(x[i] - xx) == Math.Abs(i - yy)))
+				{
+					return 0;
+				}
+			}
+			return 1;
+		}
+		static void Move(int n)
+		{
+			if (n == 8)
+			{
+				Print();
+			}
+			else
+			{
+				for (int i = 0; i < 8; i++)
+				{
+					if (Free(i, n) != 0)
+					{
+						x[n] = i;
+						Move(n + 1);
+					}
+				}
+			}
+		}
+
+	}
+}
