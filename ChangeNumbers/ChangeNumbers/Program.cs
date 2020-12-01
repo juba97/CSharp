e88c68d24896458f8360711938967da33e279a4c
juba97 <64692459+juba97@users.noreply.github.com>
2020-12-01 18:29:50 +0400

    Numbers

---
+++ ChangeNumbers/ChangeNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ChangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = ChanegeNumbers(new[] { 3, 6, 1, 9, 2 });
+            Console.WriteLine("Change numbers is");
+            foreach (var a in array)
+            {
+                Console.Write(a.ToString() + " ");
+                
+            }
+           
+        }
+        static int[] ChanegeNumbers(int[] numbers)
+        {
+            int first = numbers[0];
+            numbers[0] = numbers[numbers.Length - 1];
+            numbers[numbers.Length - 1] = first;
+
+            return numbers;
+
+            
+        }
+
+        
+    }
+}
