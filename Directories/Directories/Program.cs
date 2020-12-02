695b9383dde2a880deb8d43e414d3d6a8cf3bff9
juba97 <64692459+juba97@users.noreply.github.com>
2020-12-02 17:07:06 +0400

    GetDirectories

---
+++ Directories/Directories/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System.IO;
+namespace G03_2020_11_23
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] dirs = WriteDirectories();
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                string dir = dirs[i];
+                Console.WriteLine(dir);
+            }
+            Console.ReadKey();
+        }
+
+        static string[] WriteDirectories()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            string[] dirs = Directory.GetDirectories(@"D:\Games\Need For Speed Heat", "", SearchOption.AllDirectories);
+            return dirs;
+        }
+    }
+}
