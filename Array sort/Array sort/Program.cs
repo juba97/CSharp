6112b2530396ffa36b3b0b2adf6f610a6ee4318f
juba97 <64692459+juba97@users.noreply.github.com>
2020-12-02 23:13:41 +0400

    Sort

--- Array sort/Array sort/Program.cs
+++ Array sort/Array sort/Program.cs
@@ -1,31 +1,36 @@
 using System;
 
-namespace Array_sort {
+namespace Array_sort
+{
 
-  class Program {
-
-    static void Main(string[] args) 
+    class Program
     {
-      int[] arr = { 3, 5, 1, 4, 2 };
-      Sort(arr);
-      for (int i = 0; i < arr.Length; i++) {
-        Console.WriteLine(arr[i]);
-      }
 
-    }
-    static void Sort(int[] arr) {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 5, 1, 4, 2 };
+            Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
 
-      int temp;
-      for (int j = 0; j <= arr.Length - 2; j++) {
-        for (int i = 0; i <= arr.Length - 2; i++) {
-          if (arr[i] > arr[i + 1]) {
-            temp = arr[i + 1];
-            arr[i + 1] = arr[i];
-            arr[i] = temp;
-
-          }
         }
-      }
+        static void Sort(int[] arr)
+        {
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+        }
     }
-  }
 }
