1b448f9784b6de193ab1e8020dd735ef8ac376d7
juba97 <64692459+juba97@users.noreply.github.com>
2020-12-04 14:44:34 +0400

    Factorial

--- Factorial/Factorial/Program.cs
+++ Factorial/Factorial/Program.cs
@@ -6,17 +6,22 @@ namespace Factorial
         static void Main(string[] args)
         {
             Console.Write("Enter a Number : ");
-            int n = int.Parse(Console.ReadLine());
-            long factorial = Factorial(n);
+            uint n = uint.Parse(Console.ReadLine());
+            uint factorial = (uint)Factorial(n);
             Console.Write($"Factorial of {n} is: {factorial}");
 
             Console.ReadLine();
         }
-        static long Factorial(int n)
+        static uint Factorial(uint n)
         {
-            if (n == 1)
+             if (n == 1)
             {
                 return 1;
+               
+            }
+            if (n == 0)
+            {
+                return 0;
             }
             else
             {
