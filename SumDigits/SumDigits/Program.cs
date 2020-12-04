ee9200e82e1bdf1014d106f47729abf84db851cf
juba97 <64692459+juba97@users.noreply.github.com>
2020-12-04 17:46:40 +0400

    Sum of Digits

---
+++ SumDigits/SumDigits/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumDigits
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int sum = SumOfDigits(number);
+            Console.WriteLine($"The Sum of Digits is : {sum}");
+            Console.ReadKey();
+        }
+        static int SumOfDigits(int number)
+        {
+            if (number != 0)
+            {
+                return (number % 10 + SumOfDigits(number / 10));
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
