c9a926e404664f7b4105a98171f2ef2e1827ff48
Juba Koguashvili <64692459+juba97@users.noreply.github.com>
2021-03-15 19:28:19 +0400

    Try Catch

---
+++ Try_Catch/Try_Catch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter Number : ");
+                int N1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another Number : ");
+                int N2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(N1 / N2);
+            }
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+                Console.WriteLine("\nDone!");
+            }
+
+        }
+    }
+}
