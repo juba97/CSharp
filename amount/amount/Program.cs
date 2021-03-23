1a7060e8c6e7eb305ef0f940ce0d12c6084605f5
Juba Koguashvili <64692459+juba97@users.noreply.github.com>
2021-03-23 16:41:49 +0400

    Amount

---
+++ amount/amount/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace amount
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal amount;
+            decimal rate_of_return;
+            int years, i;
+            amount = 1000.0M;
+            rate_of_return = 0.07M;
+            years = 10;
+            Console.WriteLine("Original investment: $" + amount);
+            Console.WriteLine("Rate of return: " + rate_of_return);
+            Console.WriteLine("Over " + years + " years");
+            for (i = 0; i < years; i++)
+            {
+                amount = amount + (amount * rate_of_return);
+            }          
+            Console.WriteLine("Future value is $" + amount);
+        }
+    }
+}
