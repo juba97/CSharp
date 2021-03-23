ac850fee9d4b5182493ba631212f94ca3d1d5b7c
Juba Koguashvili <64692459+juba97@users.noreply.github.com>
2021-03-23 16:28:30 +0400

    Discount

---
+++ discount rate/discount rate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class UseDecimal
+{
+    static void Main()
+    {
+        decimal price;
+        decimal discount;
+        decimal discounted_price;
+        // Compute discounted price.
+        price = 19.95m;
+        discount = 0.15m; // discount rate is 15%
+        discounted_price = price - (price * discount);
+        Console.WriteLine("Discounted price: $" + discounted_price);
+    }
+}
