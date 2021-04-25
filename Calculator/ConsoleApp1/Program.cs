4b8b52530a27a7feb6ab087972bf34ffb92cca6f
Juba Koguashvili <64692459+juba97@users.noreply.github.com>
2021-04-25 17:04:27 +0400

    Update Program.cs

--- Calculator/ConsoleApp1/Program.cs
+++ Calculator/ConsoleApp1/Program.cs
@@ -1,42 +1,53 @@
-﻿using System;
+using System;
 class program
 {
     static void Main()
     {
         try
         {
-            Console.Write("Enter the first digit -: ");
+            Console.Write("Enter the first digit : ");
             int first = int.Parse(Console.ReadLine());
-            Console.Write("Select the operator [+, -, *, /] -: ");
+            Console.Write("Select the operator [+, -, *, /] : ");
             char symbol = char.Parse(Console.ReadLine());
-            Console.Write("Enter the second digit -: ");
+            Console.Write("Enter the second digit : ");
             int second = int.Parse(Console.ReadLine());
-            
-            
+
+
             if (symbol == '+')
             {
-                Console.Write($"Sum = {first + second}");
+                Console.WriteLine();
+                Console.Write($"Sum : {first + second}");
             }
             else if (symbol == '-')
             {
-                Console.Write($"Sub = {first - second}");
+                Console.WriteLine();
+                Console.Write($"Sub : {first - second}");
             }
             else if (symbol == '*')
             {
-                Console.Write($"Mult = {first * second}");
+                Console.WriteLine();
+                Console.Write($"Mult : {first * second}");
             }
             else if (symbol == '/')
             {
-                Console.Write($"Div = {first / second}");
+                Console.WriteLine();
+                Console.Write($"Div : {first / second}");
+            }
+            else if (symbol == '%')
+            {
+                Console.WriteLine();
+                Console.Write($"Rem : {first % second}");
             }
         }
         catch (DivideByZeroException e)
         {
+            Console.WriteLine();
             Console.WriteLine(e.Message);
         }
         catch (Exception)
         {
-            Console.WriteLine("Error");
-        }     
-    }     
+            Console.WriteLine();
+            Console.WriteLine("Unknown number or operator");
+        }
+    }
 }
